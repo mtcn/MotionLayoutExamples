593aeee748630febb898cf4d20a7cef3dd211393
Metecan Duyal <metecanduyal@gmail.com>
2018-09-24 02:26:16 +0300

    multiple animation example added

--- app/src/main/java/app/layout/motion/motionlayoutexample/ComplexAnimationActivity.kt
+++ app/src/main/java/app/layout/motion/motionlayoutexample/ComplexAnimationActivity.kt
@@ -1,35 +1,57 @@
 package app.layout.motion.motionlayoutexample
 
+import android.animation.ObjectAnimator
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.animation.BounceInterpolator
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
-import android.animation.ObjectAnimator
-import android.content.Context
-import android.view.HapticFeedbackConstants
-import android.view.animation.BounceInterpolator
-import kotlinx.android.synthetic.main.complex_animation_example.*
-import android.os.Vibrator
-
-
 
 
 class ComplexAnimationActivity: AppCompatActivity(), MotionLayout.TransitionListener {
+    private val motionLayout by lazy {
+        findViewById<MotionLayout>(R.id.motionLayout)
+    }
+    private val favImage by lazy {
+        findViewById<ImageView>(R.id.favImage)
+    }
+
+    private val seenImage by lazy {
+        findViewById<ImageView>(R.id.seenImage)
+    }
+    private val shareImage by lazy {
+        findViewById<ImageView>(R.id.shareImage)
+    }
+
+    private val mainImage2 by lazy {
+        findViewById<ImageView>(R.id.mainImage2)
+    }
+
+    var layoutId = 0
+
     override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
     }
 
     override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
         if(p0 == null)
             return
-        if(p0.currentState != -1)
+        if(layoutId == R.layout.complex_animation_example && p0.currentState != -1)
             doBounceAnimation(mainImage2)
+
+        if(layoutId == R.layout.multiple_animation_example && p0.currentState != -1){
+            doBounceAnimation(favImage)
+            doBounceAnimation(shareImage)
+            doBounceAnimation(seenImage)
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(intent.getIntExtra("layoutId", R.layout.complex_animation_example))
+        layoutId = intent.getIntExtra("layoutId", R.layout.complex_animation_example)
+        setContentView(layoutId)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -39,7 +61,6 @@ class ComplexAnimationActivity: AppCompatActivity(), MotionLayout.TransitionList
                     or View.SYSTEM_UI_FLAG_IMMERSIVE)
         }
         motionLayout.setTransitionListener(this)
-
     }
 
     private fun doBounceAnimation(targetView: View) {
--- app/src/main/java/app/layout/motion/motionlayoutexample/MainActivity.kt
+++ app/src/main/java/app/layout/motion/motionlayoutexample/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
             MainAdapter.Demo("Collapsing Toolbar w/ Text Interpolation", ExampleTypes.DEFAULT, R.layout.collapsing_toolbar_2),
             MainAdapter.Demo("Collapsing Toolbar w/ Cover Example", ExampleTypes.FULLSCREEN, R.layout.collapsing_toolbar_with_cover),
             MainAdapter.Demo("Complex Animation Example", ExampleTypes.WITHOUT_RECYCLER_VIEW, R.layout.complex_animation_example, ComplexAnimationActivity::class.java),
+            MainAdapter.Demo("Multiple Animation Example", ExampleTypes.FULLSCREEN, R.layout.multiple_animation_example, ComplexAnimationActivity::class.java),
             MainAdapter.Demo("Basic Keyframe Example", ExampleTypes.WITHOUT_RECYCLER_VIEW, R.layout.basic_key_frame_example),
             MainAdapter.Demo("Basic Keyframe Example 2", ExampleTypes.WITHOUT_RECYCLER_VIEW, R.layout.basic_key_frame_example_2),
             MainAdapter.Demo("Notification Center Example", ExampleTypes.DEFAULT, R.layout.notification_center_example),
@@ -44,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun start(activity: Class<*>, layoutFileId: Int, types: ExampleTypes?, position: Int) {
-        if (position > 3) {
+        if (position > 4) {
             Toast.makeText(this, "Coming soon...", Toast.LENGTH_LONG).show()
         } else {
             val intent = Intent(this, activity).apply {
